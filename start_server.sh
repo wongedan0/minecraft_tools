3ba64eec7d87a9ddbd8a28541454a0b7b1b39b62
Artemis Rosman <73006620+rozukke@users.noreply.github.com>
2024-09-04 16:07:00 +1000

    Add nogui to linux ver

--- start_server.sh
+++ start_server.sh
@@ -1,2 +1,2 @@
 cd server
-java -Xms512M -Xmx1024M -jar -DIReallyKnowWhatIAmDoingISwear spigot-1.19.4.jar -o true
+java -Xms512M -Xmx1024M -jar -DIReallyKnowWhatIAmDoingISwear spigot-1.19.4.jar -o true nogui
